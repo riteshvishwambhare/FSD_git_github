471e3d61451dcc36b50d7d3ced3fce8633caacb3
riteshvishwambhare <riteshvishwambhare@gmail.com>
2025-08-01 17:36:03 +0530

    adding newBranch

---
+++ second.java
@@ -0,0 +1,8 @@
+public class second {
+    public static void main(String[] args){
+        
+        system.out.println("calling second file into main method");
+         system.out.println("calling second file into main method");
+         System.out.println("for create new branch");
+    }
+}
